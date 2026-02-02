37a9d8c92424b995911d678af313cfbdbbfa9268
Shivendu Kumar <shiv.end.you@gmail.com>
2026-02-02 22:22:35 +0530

    add: v1 home api

---
+++ api/v1/home.py
@@ -0,0 +1,48 @@
+# app/api/v1/home.py
+
+from fastapi import APIRouter, HTTPException
+from services.article_service import ArticleService
+from schemas.article import ArticleSummary, TrendingArticle
+
+router = APIRouter(prefix="/home", tags=["Home"])
+
+
+@router.get("/main-article", response_model=ArticleSummary)
+async def main_article():
+    article = await ArticleService.get_latest_article_any_domain()
+    if not article:
+        raise HTTPException(status_code=404, detail="No articles found")
+    return article
+
+
+@router.get("/related-articles", response_model=list[ArticleSummary])
+async def related_articles(
+    exclude_slug: str,
+    limit: int = 8
+):
+    return await ArticleService.get_home_related_articles(
+        exclude_slug=exclude_slug,
+        limit=limit
+    )
+
+
+@router.get("/tech-barometer")
+async def tech_barometer():
+    data = await ArticleService.get_tech_barometer()
+
+    return [
+        {
+            "domain": d["_id"],
+            "label": d["category"]["name"],
+            "score": d["score"]
+        }
+        for d in data
+    ]
+
+
+@router.get("/trending", response_model=list[TrendingArticle])
+async def trending_by_domain(domain_slug: str, limit: int = 5):
+    return await ArticleService.get_trending_by_domain(
+        domain_slug=domain_slug,
+        limit=limit
+    )
