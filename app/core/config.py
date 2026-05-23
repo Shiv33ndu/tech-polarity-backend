d45bab0780d676c86397dc6af984482104ed52f1
Shivendu Kumar <shiv.end.you@gmail.com>
2026-05-24 01:16:41 +0530

    added: admin panel url

--- app/core/config.py
+++ app/core/config.py
@@ -28,6 +28,7 @@ class Settings(BaseSettings):
     ALLOWED_ORIGINS: list[str] = [
         "http://localhost:3000",
         "http://localhost:9002",
+        "https://admin-techpolarity.netlify.app/login",
         "https://tech-polarity-2026.vercel.app",
     ]
 
